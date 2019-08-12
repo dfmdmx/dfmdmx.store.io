7f96b7c2884d2f490ae06a583d4824e416f5d5f4
dandfmd <daniel@dfmd.mx>
2019-08-12 14:06:52 -0500

    Google logout

--- assets/main.js
+++ assets/main.js
@@ -104,8 +104,11 @@ function logout() {
 	if (session_token != '') {
 	 return remoteCall('user_logout',{'session_token':session_token}).then(function(response){
 		  setCookie('session_token','',-1000);
-			GoogleSignOut();
-			//location.reload();
+			var auth2 = gapi.auth2.getAuthInstance();
+			auth2.signOut().then(function () {
+				console.log('User signed out.');
+				location.reload();
+			});
 		}).fail(function(){
 			setCookie('session_token','',-1000);
 			location.reload();
@@ -113,13 +116,6 @@ function logout() {
 	} else {
 		return false;
 	};
-}
-
-function GoogleSignOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut().then(function () {
-		console.log('User signed out.');
-	});
 }
 
 // Se necesita colocar css y html en header para que esto funcione
