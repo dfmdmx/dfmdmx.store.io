fdec4f3a133b84d3ec449683fc96428a44062cb7
dandfmd <daniel@dfmd.mx>
2019-08-12 00:25:13 -0500

    dwv

--- assets/main.js
+++ assets/main.js
@@ -104,6 +104,7 @@ function logout() {
 	if (session_token != '') {
 	 return remoteCall('user_logout',{'session_token':session_token}).then(function(response){
 		  setCookie('session_token','',-1000);
+			var auth2 = gapi.auth2.getAuthInstance();
 			auth2.signOut().then(function () {
 				console.log('Google user signed out.');
 			});
