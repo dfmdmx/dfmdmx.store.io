9b822f71b252eea3f503885fef4f04c07f2e6b3d
dandfmd <daniel@dfmd.mx>
2019-08-12 00:39:00 -0500

    dev

--- assets/main.js
+++ assets/main.js
@@ -100,12 +100,10 @@ function getUser() {
 }
 
 function signOutGoogle() {
-	$(document).ready(function () {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-	});
 }
 
 function logout() {
