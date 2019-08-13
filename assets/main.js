a9df806cd57e14020efaa29c15dfd88ca42bdbc2
dandfmd <daniel@dfmd.mx>
2019-08-13 14:08:32 -0500

    Still on google signin

--- assets/main.js
+++ assets/main.js
@@ -106,7 +106,8 @@ function logout() {
 		  setCookie('session_token','',-1000);
 			var auth2 = gapi.auth2.getAuthInstance();
 			auth2.signOut().then(function () {
-				console.log('User signed out.');
+				window.location.replace("/");
+			}).fail(function(){
 				window.location.replace("/");
 			});
 		}).fail(function(){
