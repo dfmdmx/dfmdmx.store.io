3c4fc747745db5adb127db753c4bc29e79d59be8
dandfmd <daniel@dfmd.mx>
2019-10-28 20:19:04 -0600

    edit order

--- assets/main.js
+++ assets/main.js
@@ -133,3 +133,16 @@ var loader = {
 		jQuery( '.loading-image' ).toggleClass("stop");
 	}
 }
+
+function formatCurrency(total) {
+    var neg = false;
+    if(total < 0) {
+        neg = true;
+        total = Math.abs(total);
+    }
+    return (neg ? "-$" : '$') + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();
+}
+
+function parseCurrency(currency) {
+    return parseFloat(currency.replace(',','').replace('$',''))
+}
